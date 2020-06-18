607d0bd317de8add71e6e0ed984d73d054258c42
dhineshhh985 <dhineshhh985@gmail.com>
2020-06-18 18:21:21 +0530

    urls updated

--- Jobs_site/urls.py
+++ Jobs_site/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('jobs_app/',include('jobs_app.urls')),
+    path('',include('jobs_app.urls')),
     path('admin/', admin.site.urls),
     
 ]
